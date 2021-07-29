81751d657f15e2b60108df256c7187865916e6d8
althafabbas <57086045+keralapesleague6@users.noreply.github.com>
2021-07-29 17:38:38 +0530

    first commit

---
+++ js/vote.js
@@ -0,0 +1,6 @@
+// let button = document.getElementById('vote')
+let dontGet = document.getElementById('not-display')
+
+function error() {
+    dontGet.innerText = "this option is currentlt un available"
+}
